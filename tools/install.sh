441ceafae7ce49e2480acb8ba99990e178d0f5fc
Zenki.J.Zha <Zenki.J.Zha@foxconn.com>
2012-05-11 21:00:15 +0800

    comment robb's oh-my-zsh git

--- tools/install.sh
+++ tools/install.sh
@@ -5,18 +5,18 @@ then
 fi
 
 echo "\033[0;34mCloning Oh My Zsh...\033[0m"
-/usr/bin/env git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+# /usr/bin/env git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 echo "\033[0;34mLooking for an existing zsh config...\033[0m"
 if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]
 then
   echo "\033[0;33mFound ~/.zshrc.\033[0m \033[0;32]Backing up to ~/.zshrc.pre-oh-my-zsh\033[0m";
-  cp ~/.zshrc ~/.zshrc.pre-oh-my-zsh;
+  # cp ~/.zshrc ~/.zshrc.pre-oh-my-zsh;
   rm ~/.zshrc;
 fi
 
 echo "\033[0;34mUsing the Oh My Zsh template file and adding it to ~/.zshrc\033[0m"
-cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+ln -s ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
 echo "\033[0;34mCopying your current PATH and adding it to the end of ~/.zshrc for you.\033[0m"
 echo "export PATH=$PATH" >> ~/.zshrc
